3d9dde91f4d1069c52b645044bf2d653424cca3b
werivik <wervik00994@stud.noroff.no>
2024-11-30 23:10:49 +0100

    responsive

--- js/nav/account.js
+++ js/nav/account.js
@@ -5,18 +5,28 @@ const loginOptions = document.querySelector('.login-options');
 const userProfile = document.querySelector('.user-profile');
 const creditAmount = document.querySelector('.credit-amount p');
 
+const loginOptions2 = document.querySelector('.login-options2');
+const userProfile2 = document.querySelector('.user-profile2');
+const creditAmount2 = document.querySelector('.credit-amount2 p');
+const avatarImg = document.querySelector('.avatar img');
+const avatarImg2 = document.querySelector('.avatar2 img');
+
 const token = localStorage.getItem('accessToken') || sessionStorage.getItem('accessToken');
 
 function checkLoginStatus() {
     if (token) {
         loginOptions.style.display = 'none';
         userProfile.style.display = 'flex';
+        loginOptions2.style.display = 'none';
+        userProfile2.style.display = 'flex';
         fetchUserData();
     } 
     
     else {
         loginOptions.style.display = 'flex';
         userProfile.style.display = 'none';
+        loginOptions2.style.display = 'flex';
+        userProfile2.style.display = 'none';
     }
 }
 
@@ -37,8 +47,24 @@ async function fetchUserData() {
 
         const data = await response.json();
         const credits = data.data.credits || 0;
+        const avatar = data.data.avatar?.url;
 
         creditAmount.textContent = `$ ${credits} Credits`;
+        creditAmount2.textContent = `$ ${credits} Credits`;
+
+        if (avatar) {
+            avatarImg.src = avatar;
+            avatarImg.alt = 'User Avatar';
+            avatarImg2.src = avatar;
+            avatarImg2.alt = 'User Avatar';
+        } 
+        
+        else {
+            avatarImg.src = '/media/default-profile.jpg';
+            avatarImg.alt = 'Default Avatar';
+            avatarImg2.src = '/media/default-profile.jpg';
+            avatarImg2.alt = 'Default Avatar';
+        }
     } 
     
     catch (error) {
