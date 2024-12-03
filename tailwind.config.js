2d8fbd65203e7232aa5687936f2b1b5ca680ab85
werivik <wervik00994@stud.noroff.no>
2024-12-03 14:52:49 +0100

    no issues yet

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./auth/**/*.{html,js}",
+    "./listings/**/*.{html,js}",
+    "./profile/**/*.{html,js}",
+    "./js/**/*.js",
+    "./index.html",
+    "./listings/all-listings.html",
+    "./profile/profile.html",
+    "./media/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
