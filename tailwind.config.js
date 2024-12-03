22bbce208854340dad6b69cb92d58729a97f38a8
werivik <wervik00994@stud.noroff.no>
2024-12-03 22:39:08 +0100

    index tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,43 @@ module.exports = {
     './src/**/*.css',
     './src/css/**/*.css',
   ],
+  safelist: [
+    "text-font1",
+    "text-font2",
+    "text-font3",
+  ],
   theme: {
     extend: {
       colors: {
         bodyBackground: '#EBEEE9',
         darkGreyFont: '#29272F',
         lightFont: '#F2F2F2',
-        green: '#81AB76',
-        orange: '#D6650F',
+        specialGreen: '#81AB76',
+        specialOrange: '#D6650F',
       },
+      backgroundImage: {
+        'grain-texture': "url('/media/textures/grain-texture2.png')",
+      },
+      fontFamily: {
+        serif: ['"Libre Bodoni"', 'serif'],
+      },
+      fontSize: {
+        font1: "35px",
+        font2: "30px",
+        font3: "25px",
+        font4: "18px",
+      },
+      padding: {
+        sPaddingUp: "20px",
+        mPaddingUp: "40px",
+        lPaddingUp: "80px",
+        xlPaddingUp: "160px",
+
+        sPaddingDown: "20px",
+        mPaddingDown: "40px",
+        lPaddingDown: "80px",
+        xlPaddingDown: "160px",
+      }
     },
   },
   plugins: [],
