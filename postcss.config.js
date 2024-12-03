8bb508808cd082febe6e4dc7195b16ec922383fd
werivik <wervik00994@stud.noroff.no>
2024-12-03 15:25:15 +0100

    please, for once WORK

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-    },
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./auth/**/*.{html,js}",
-    "./listings/**/*.{html,js}",
-    "./profile/**/*.{html,js}",
-    "./js/**/*.js",
-    "./index.html",
-    "./listings/all-listings.html",
-    "./profile/profile.html",
-    "./media/**/*.{html,js}",
+    './src/**/*.html',
+    './src/css/**/*.css',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fontColorDark: '#29272F',
+        fontColorLight: '#F2F2F2',
+        bodyColor: '#EBEEE9',
+        orangeColor: '#D6650F',
+        blackColor: '#29272F',
+        greenColor: '#81AB76',
+      },
+      fontFamily: {
+        serif:["Libre Bodoni", serif]
+      },
+    },
   },
   plugins: [],
 }
